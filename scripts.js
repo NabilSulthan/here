a139b9cbfab87a75a1bccec3a4a7a7e65c3aaa86
NABIL SULTHAN <109164712+NabilSulthan@users.noreply.github.com>
2024-11-10 13:03:28 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Function to display the hidden message
+function showMessage() {
+    var message = document.getElementById("specialMessage");
+
+    // Remove the hidden class to display the message
+    message.classList.remove("hidden");
+}
